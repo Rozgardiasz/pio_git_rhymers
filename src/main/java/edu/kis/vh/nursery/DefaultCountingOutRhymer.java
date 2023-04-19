e80afc0e568db48f601eee547ca26502d763c63f
Rozgardiasz <97686873+Rozgardiasz@users.noreply.github.com>
2023-04-19 12:38:36 +0200

    Revert "zmiany zmiany"

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,12 +2,9 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int MAX_SIZE = 12;
-    public static final int EMPTY = -1;
-    public static final int FULL = 11;
-    private final int[] numbers = new int[MAX_SIZE];
+    private int[] numbers = new int[12];
 
-    public int total = EMPTY;
+    public int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
@@ -15,22 +12,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == EMPTY;
+        return total == -1;
     }
 
     public boolean isFull() {
-        return total == FULL;
+        return total == 11;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return EMPTY;
+            return -1;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return EMPTY;
+            return -1;
         return numbers[total--];
     }
 
